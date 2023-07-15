8dd5f3838a3f7fe0b3a6a4857b4d061afb630828
Niklas Ladehoff <business-mail-niklas@web.de>
2023-07-15 23:57:45 +0200

    add gsap

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import "../styles/globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Roboto } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["100", "300", "400", "500", "700", "900"],
+});
 
 export const metadata: Metadata = {
   title: "Niklas-dev | Portfolio",
@@ -16,7 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={roboto.className}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,43 @@
+"use client";
 import ParticlesBackground from "@/components/ParticlesBackground";
+import { gsap } from "gsap";
+import { useEffect, useLayoutEffect } from "react";
 
 export default function Home() {
+  useLayoutEffect(() => {
+    const timeline = gsap.timeline({
+      delay: 0.3,
+    });
+
+    timeline.to(".word", {
+      duration: 0.3,
+      opacity: 1,
+      scale: 1,
+      ease: "power2.easeOut",
+      stagger: {
+        amount: 1.2,
+      },
+    });
+
+    return () => {};
+  }, []);
+
   return (
-    <main className="h-screen w-screen">
+    <main className="h-screen w-screen ">
       <ParticlesBackground />
-      <div className="text-white"></div>
+      <div className="text-white h-full w-full flex flex-col items-center justify-center gradient">
+        <div>
+          <h1 className="text-8xl font-bold word opacity-0 scale-0">
+            Niklas-dev
+          </h1>
+          <h1 className="text-8xl font-bold word opacity-0 scale-0">
+            Niklas-dev
+          </h1>
+          <h1 className="text-8xl font-bold word opacity-0 scale-0">
+            Niklas-dev
+          </h1>
+        </div>
+      </div>
     </main>
   );
 }
