c113eafbf8cd3594c32a304164e23f20269731c4
Niklas Ladehoff <business-mail-niklas@web.de>
2023-07-16 03:28:07 +0200

    remove ununsed imports

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,4 @@
-"use client";
-import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
+import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 export default function NavBar() {
