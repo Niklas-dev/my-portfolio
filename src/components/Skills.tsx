4220bcf831e43a8d666ce5a12df70fe02bba97de
Niklas Ladehoff <business-mail-niklas@web.de>
2023-08-21 19:31:12 +0200

    fix soft skills part

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -6,7 +6,7 @@ export default function Skills() {
       <h3 className="bg-transparent text-firstwhite text-xl font-medium pt-2">
         Technologies
       </h3>
-      <div className="flex flex-row flex-wrap justify-start md:justify-end bg-transparent gap-2">
+      <div className="flex flex-row flex-wrap justify-start lg:justify-end bg-transparent gap-2">
         <div className="bg-[#1e1e1e] shadow-md drop px-4 py-2 rounded-xl text-firstwhite">
           Django
         </div>
--- src/components/SoftSkills.tsx
+++ src/components/SoftSkills.tsx
@@ -6,7 +6,7 @@ export default function SoftSkills() {
       <h3 className="bg-transparent text-firstwhite text-xl font-medium pt-2">
         Soft Skills
       </h3>
-      <div className="flex flex-row bg-transparent gap-2">
+      <div className="flex flex-row justify-start lg:justify-end flex-wrap bg-transparent gap-2">
         <div className="bg-[#1e1e1e] shadow-md drop px-4 py-2 rounded-xl text-firstwhite">
           teamplayer
         </div>
@@ -16,16 +16,12 @@ export default function SoftSkills() {
         <div className="bg-[#1e1e1e] shadow-md drop px-4 py-2 rounded-xl text-firstwhite">
           problem solver
         </div>
-      </div>
-      <div className="flex flex-row bg-transparent gap-2">
         <div className="bg-[#1e1e1e] shadow-md drop px-4 py-2 rounded-xl text-firstwhite">
           communications
         </div>
         <div className="bg-[#1e1e1e] shadow-md drop px-4 py-2 rounded-xl text-firstwhite">
           analytical thinking
         </div>
-      </div>
-      <div className="flex flex-row bg-transparent gap-2">
         <div className="bg-[#1e1e1e] shadow-md drop px-4 py-2 rounded-xl text-firstwhite">
           empathy
         </div>
